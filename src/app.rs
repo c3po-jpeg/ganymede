18f1c33c000f2a5dac20b526a598a36a71c1e2dc
c3po-jpeg <vickkaniu0@gmail.com>
2026-06-21 14:46:47 +0300

    window creation

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,55 @@
+use winit::{application::ApplicationHandler, window::Window};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(
+                    Window::default_attributes()
+                        .with_title("ganymede")
+                        .with_inner_size(winit::dpi::PhysicalSize::new(800, 600)),
+                )
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            winit::event::WindowEvent::CloseRequested => {
+                println!("closing app...");
+                event_loop.exit();
+            }
+            winit::event::WindowEvent::RedrawRequested => {
+                // rendering code would go here
+            }
+            winit::event::WindowEvent::KeyboardInput { .. } => {
+                // input handling code would go here
+            }
+
+            winit::event::WindowEvent::MouseInput { .. } => {
+                // mouse input handling code would go here
+            }
+            _ => {}
+        }
+    }
+}
 
 pub fn run() -> anyhow::Result<()> {
-    println!("running...");
+    #[cfg(target_os = "linux")]
+    unsafe {
+        std::env::set_var("WAYLAND_DISPLAY", "");
+    }
+    let event_loop = winit::event_loop::EventLoop::new()?;
+    event_loop.run_app(&mut App::default())?;
     Ok(())
 }
