ce2d39368fd372a5c8b96316201599b696109356
c3po-jpeg <vickkaniu0@gmail.com>
2026-06-26 10:37:11 +0300

    more minor changes

--- src/scene.rs
+++ src/scene.rs
@@ -1,13 +1,19 @@
-use crate::entity::Entity;
+use crate::{buffer::Buffer, camera::Camera, entity::Entity};
 
 pub struct Scene {
     entities: Vec<Entity>,
+    camera: Camera,
+    camera_buffer: Option<Buffer>,
 }
 
 impl Scene {
     pub fn new() -> Self {
+        let camera = Camera::new(800.0 / 600.0);
+
         Self {
             entities: Vec::new(),
+            camera,
+            camera_buffer: None,
         }
     }
 
@@ -20,6 +26,8 @@ impl Scene {
     pub fn add_entity(&mut self, entity: Entity) {
         self.entities.push(entity);
     }
+
+    pub fn update(&mut self, dt: f32, aspect_ratio: f32) {}
 
     pub fn render(&self, render_pass: &mut wgpu::RenderPass<'_>) -> anyhow::Result<()> {
         for entity in &self.entities {
